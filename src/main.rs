9438fd813ad49fc3a28d1b88d89edaf3a6749326
Roger Filmyer <roger.filmyer@gmail.com>
2021-02-08 01:59:22 -0500

    we have a spectrogram

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, fs::File};
+use std::io::BufReader;
+use rodio::{self, Source};
+use sonogram::SpecOptionsBuilder;
+
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("Aphex Twin - Windowlicker.mp3")?;
+
+    let source = rodio::Decoder::new(BufReader::new(file))?;
+    println!("Read file");
+    
+    let sample_rate = source.sample_rate();
+    let waveform: Vec<i16> = source.into_iter().collect();
+    println!("Converted file into waveform");
+    
+    let mut spectrograph = SpecOptionsBuilder::new(1024 * 8,256)
+        .load_data_from_memory(waveform, sample_rate)
+        .build();
+
+    spectrograph.compute(2048, 0.8);
+    println!("Computed spectrograph");
+
+    let png_file = std::path::Path::new("windowlicker.png");
+    spectrograph.save_as_png(&png_file, true)?;
+    println!("Saved as PNG");
+
+    Ok(())
 }
